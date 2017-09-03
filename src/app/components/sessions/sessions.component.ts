68779c27ed71b230c1b4b69687fd4b4075fc7fd3
cristianolaya <warrdnez@gmail.com>
2017-09-02 22:30:46 -0500

    adding validations

--- src/app/components/sessions/sessions.component.ts
+++ src/app/components/sessions/sessions.component.ts
@@ -24,10 +24,171 @@ export class SessionsComponent implements OnInit {
   };
 
   getTasks(){
+    var self = this;
+    this.setGroup();
     let muscles = JSON.stringify(this.session.data.muscle.split('-'));
+    this.session.list = [];
+
     this.session.getEjercicesByMuscle(muscles).then(response => {
-      console.log(response);
+      self.session.fullData = response.data;
+      self.updateArrays();
+      self.selectExercises();
+      console.log(this.session.list);
     });
+  };
+
+  addElements(indice: number, list: any){
+    var cantidad: number = this.session.group[indice];
+    var total = list.length;
+    var arr = [];
+
+    if(cantidad <= list.length){
+      while(arr.length < cantidad){
+        var randomnumber = Math.floor((Math.random() * total) + 1) - 1;
+        if(arr.indexOf(randomnumber) > -1) continue;
+        arr[arr.length] = randomnumber;
+      }
+
+      for(var j = 0; j < arr.length; j++){
+        this.session.list.push(list[arr[j]]);
+      }
+    }else {
+      console.log("Hay un error");
+    }
+  };
+
+  selectExercises(){
+    let muscles = this.session.data.muscle.split('-');
+
+    for(let i = 0; i < muscles.length; i++) {
+      let exercise =  muscles[i];
+
+      switch(exercise) {
+        case 'Pecho':
+          var quantity = this.session.index.indexOf('Pecho');
+          this.addElements(quantity, this.session.pecho);
+
+          break;
+
+        case 'Espalda':
+          var quantity = this.session.index.indexOf('Espalda');
+          this.addElements(quantity, this.session.espalda);
+          break;
+
+        case 'Muslos':
+          var quantity = this.session.index.indexOf('Muslos');
+          this.addElements(quantity, this.session.muslo);
+          break;
+
+        case 'Pantorrilla':
+          var quantity = this.session.index.indexOf('Pantorrilla');
+          this.addElements(quantity, this.session.pantorrilla);
+          break;
+
+        case 'Bíceps':
+          var quantity = this.session.index.indexOf('Bíceps');
+          this.addElements(quantity, this.session.biceps);
+          break;
+
+        case 'Tríceps':
+          var quantity = this.session.index.indexOf('Tríceps');
+          this.addElements(quantity, this.session.triceps);
+          break;
+
+        case 'Hombros':
+          var quantity = this.session.index.indexOf('Hombros');
+          this.addElements(quantity, this.session.hombro);
+          break;
+
+        case 'Trapecio':
+          var quantity = this.session.index.indexOf('Trapecio');
+          this.addElements(quantity, this.session.trapecio);
+          break;
+
+        case 'Abdomen':
+          var quantity = this.session.index.indexOf('Abdomen');
+          this.addElements(quantity, this.session.abdomen);
+          break;
+      }
+    }
+  };
+
+  clearArrays(){
+    this.session.pecho = [];
+    this.session.espalda = [];
+    this.session.muslo = [];
+    this.session.pantorrilla = [];
+    this.session.biceps = [];
+    this.session.triceps = [];
+    this.session.hombro = [];
+    this.session.trapecio = [];
+    this.session.abdomen = [];
+  };
+
+  updateArrays(){
+    this.clearArrays();
+
+    for(let i = 0; i < this.session.fullData.length; i++) {
+      let exercise =  this.session.fullData[i];
+
+      switch(exercise.musculo) {
+        case 'Pecho':
+          this.session.pecho.push(exercise);
+          break;
+
+        case 'Espalda':
+          this.session.espalda.push(exercise);
+          break;
+
+        case 'Muslos':
+          this.session.muslo.push(exercise);
+          break;
+
+        case 'Pantorrilla':
+          this.session.pantorrilla.push(exercise);
+          break;
+
+        case 'Bíceps':
+          this.session.biceps.push(exercise);
+          break;
+
+        case 'Tríceps':
+          this.session.triceps.push(exercise);
+        break;
+
+        case 'Hombros':
+          this.session.hombro.push(exercise);
+          break;
+
+        case 'Trapecio':
+          this.session.trapecio.push(exercise);
+          break;
+
+        case 'Abdomen':
+          this.session.abdomen.push(exercise);
+          break;
+      }
+    }
+  };
+
+  setGroup(){
+    if(this.session.data.gender === 'Hombre'){
+      if(this.session.data.time === '12 o más'){
+        this.session.group = JSON.parse(this.session.groupC);
+      }else if(this.session.data.time === "0 - 5 meses") {
+        this.session.group = JSON.parse(this.session.groupA);
+      }else {
+        this.session.group = JSON.parse(this.session.groupB);
+      }
+    }else if(this.session.data.gender === 'Mujer'){
+      if(this.session.data.time === '12 o más'){
+        this.session.group = JSON.parse(this.session.groupE);
+      }else if(this.session.data.time === "0 - 5 meses") {
+        this.session.group = JSON.parse(this.session.groupD);
+      }else {
+        this.session.group = JSON.parse(this.session.groupD);
+      }
+    }
   };
 
   presentModal() {
--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -7,57 +7,27 @@ import 'rxjs/add/operator/catch';
 export class SessionService {
   private baseUrl = 'http://localhost:8080';
   //private baseUrl = 'http://ec2-35-162-194-240.us-west-2.compute.amazonaws.com:8080';
-  public dataItems = {
-    genders: [
-      {name: 'Hombre', id: 0},
-      {name: 'Mujer', id: 1}
-    ],
-    times: [
-      {name: '0 - 5 meses', id: 0},
-      {name: '6 - 11 meses', id: 1},
-      {name: '12 o más', id: 2}
-    ],
-    targets: [
-      {name: 'Aumentar masa', id: 0},
-      {name: 'Tonificar', id: 1},
-      {name: 'Bajar de peso', id: 2}
-    ],
-    places: [
-      {name: 'Casa', id: 0},
-      {name: 'Gimnasio', id: 1}
-    ],
-    muscles: []
-  };
-
-  public muscles = [
-    [
-      {name: 'Pecho-Bíceps-Abdomen', id: 0},
-      {name: 'Espalda-Tríceps-Abdomen', id: 1},
-      {name: 'Muslos-Abdomen', id: 2},
-      {name: 'Pantorilla-Abdomen', id: 3},
-      {name: 'Hombro-Trapecio-Abdomen', id: 4}
-    ],
-    [
-      {name: 'Pecho-Abdomen', id: 0},
-      {name: 'Espalda-Abdomen', id: 1},
-      {name: 'Biceps-Tríceps-Abdomen', id: 2},
-      {name: 'Hombro-Trapecio-Abdomen', id: 3},
-      {name: 'Muslo-Pantorrilla-Abdomen', id: 4}
-    ],
-    [
-      {name: 'Muslos-Pantorrilla-Abdomen', id: 0},
-      {name: 'Pecho-Tríceps-Bíceps-Abdomen', id: 0},
-      {name: 'Espalda-Hombro-Abdomen', id: 0}
-    ]
-  ];
-
-  public data = {
-    gender: '',
-    time: '',
-    target: '',
-    place: '',
-    muscle: ''
-  };
+  public dataItems = {genders:[{name:'Hombre',id:0},{name:'Mujer',id:1}],times:[{name:'0 - 5 meses',id:0},{name:'6 - 11 meses',id:1},{name:'12 o más',id:2}],targets:[{name:'Aumentar masa',id:0},{name:'Tonificar',id:1},{name:'Bajar de peso',id:2}],places:[{name:'Casa',id:0},{name:'Gimnasio',id:1}],muscles:[]};
+  public muscles = [[{name:'Pecho-Bíceps-Abdomen',id:0},{name:'Espalda-Tríceps-Abdomen',id:1},{name:'Muslos-Abdomen',id:2},{name:'Pantorilla-Abdomen',id:3},{name:'Hombros-Trapecio-Abdomen',id:4}],[{name:'Pecho-Abdomen',id:0},{name:'Espalda-Abdomen',id:1},{name:'Biceps-Tríceps-Abdomen',id:2},{name:'Hombros-Trapecio-Abdomen',id:3},{name:'Muslos-Pantorrilla-Abdomen',id:4}],[{name:'Muslos-Pantorrilla-Abdomen',id:0},{name:'Pecho-Tríceps-Bíceps-Abdomen',id:0},{name:'Espalda-Hombros-Abdomen',id:0}]];
+  public data = {gender:'',time:'',target:'',place:'',muscle:''};
+  public groupA = '[ 3, 3, 4, 1, 2, 2, 4, 1, 2 ]';
+  public groupB = '[ 4, 4, 4, 2, 3, 3, 4, 2, 2 ]';
+  public groupC = '[ 5, 5, 5, 2, 4, 4, 5, 2, 2 ]';
+  public groupD = '[ 3, 3, 4, 1, 2, 2, 3, 2 ]';
+  public groupE = '[ 3, 3, 5, 1, 2, 2, 3, 2 ]';
+  public index = ['Pecho','Espalda','Muslos','Pantorrilla','Bíceps','Tríceps','Hombros','Trapecio','Abdomen'];
+  public group = [];
+  public fullData = [];
+  public pecho = [];
+  public espalda = [];
+  public muslo = [];
+  public pantorrilla = [];
+  public biceps = [];
+  public triceps = [];
+  public hombro = [];
+  public trapecio = [];
+  public abdomen = [];
+  public list = [];
 
   constructor(private http: Http) { };
 
