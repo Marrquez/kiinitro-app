89213cbf66e9ad1e7e9f44f3a3f82c2b7e52130d
cristianolaya <warrdnez@gmail.com>
2017-09-10 13:13:40 -0500

    hidding home option

--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/catch';
 export class SessionService {
   private baseUrl = 'http://localhost:8080';
   //private baseUrl = 'http://ec2-35-162-194-240.us-west-2.compute.amazonaws.com:8080';
-  public dataItems = {genders:[{name:'Hombre',id:0},{name:'Mujer',id:1}],times:[{name:'0 - 5 meses',id:0},{name:'6 - 11 meses',id:1},{name:'12 o más',id:2}],targets:[{name:'Aumentar masa',id:0},{name:'Tonificar',id:1},{name:'Bajar de peso',id:2}],places:[{name:'Casa',id:0},{name:'Gimnasio',id:1}],muscles:[]};
+  public dataItems = {genders:[{name:'Hombre',id:0},{name:'Mujer',id:1}],times:[{name:'0 - 5 meses',id:0},{name:'6 - 11 meses',id:1},{name:'12 o más',id:2}],targets:[{name:'Aumentar masa',id:0},{name:'Tonificar',id:1},{name:'Bajar de peso',id:2}],places:[/*{name:'Casa',id:0},*/{name:'Gimnasio',id:1}],muscles:[]};
   public muscles = [
     [
       {name:'Pecho-Bíceps-Abdomen',id:0},
