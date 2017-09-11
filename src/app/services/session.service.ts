56e01a62e49631ebc718bd5f683ca39f8e0268b5
marrquez <marrquez@hotmail.com>
2017-09-11 15:26:12 -0500

    fix, the amazon server doesnt work

--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class SessionService {
   //private baseUrl = 'http://localhost:8080';
-  private baseUrl = 'http://ec2-35-162-194-240.us-west-2.compute.amazonaws.com:8080';
+  private baseUrl = 'http://52.42.164.164:4500';
   public dataItems = {genders:[{name:'Hombre',id:0},{name:'Mujer',id:1}],times:[{name:'0 - 5 meses',id:0},{name:'6 - 11 meses',id:1},{name:'12 o más',id:2}],targets:[{name:'Aumentar masa',id:0},{name:'Tonificar',id:1},{name:'Bajar de peso',id:2}],places:[/*{name:'Casa',id:0},*/{name:'Gimnasio',id:1}],muscles:[]};
   public muscles = [
     [
@@ -53,7 +53,7 @@ export class SessionService {
   constructor(private http: Http) { };
 
   public getEjercicio(id: string) {
-    let url = this.baseUrl + '/getEjercicio';
+    let url = this.baseUrl + '/get-ejercicio';
     let params: URLSearchParams = new URLSearchParams();
 
     params.set('id', id);
@@ -65,7 +65,7 @@ export class SessionService {
   };
 
   public getEjercicesByMuscle(muscles: string) {
-    let url = this.baseUrl + '/getEjercicesByMuscle';
+    let url = this.baseUrl + '/get-ejercicesByMuscle';
     let params: URLSearchParams = new URLSearchParams();
 
     params.set('muscles', muscles);
