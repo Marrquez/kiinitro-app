9ef5cee41a851649d02be4dfc575307ee723a1fb
warrdnez <Bizagi2016>
2017-09-13 11:48:25 -0500

    adding the new about us page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'about',
   templateUrl: 'about.component.html'
 })
 export class AboutComponent {
-  constructor(public navCtrl: NavController) { }
+  @ViewChild(Slides) slides: Slides;
+  constructor(public navCtrl: NavController) { };
+
+  ngAfterViewInit() {
+    this.slides.autoplay = 1000;
+    this.slides.loop = true;
+  }
 }
