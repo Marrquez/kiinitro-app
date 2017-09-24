8f69cc3b46170a9ec430842c57bf3e908b89ae0a
cristianolaya <warrdnez@gmail.com>
2017-09-24 11:29:32 -0500

    adding loggin w google with redirect

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -28,7 +28,7 @@ export class AuthService {
   logByGoogle(){
     var self = this;
     var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
+    firebase.auth().signInWithRedirect(provider).then(function(result) {
       // This gives you a Google Access Token. You can use it to access the Google API.
       var token = result.credential.accessToken;
       // The signed-in user info.
