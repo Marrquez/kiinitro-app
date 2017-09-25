7335c8dc03bfbafa56b2824c84df9ed5f8f36a9b
cristianolaya <warrdnez@gmail.com>
2017-09-24 22:07:37 -0500

    adding change points button

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -3,7 +3,7 @@ import { IonicPage,
   NavController,
   Loading,
   LoadingController,
-  AlertController } from 'ionic-angular';
+  AlertController, Nav, Tabs } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorService } from '../../services/validator.service';
 import { AuthService } from '../../services/auth.service';
@@ -14,11 +14,13 @@ import { SessionComponent } from '../session/session.component';
   templateUrl: 'sign-up.component.html',
 })
 export class SignUpComponent {
+  tab:Tabs;
   public signupForm: FormGroup;
   loading: Loading;
   constructor(public navCtrl: NavController, public auth: AuthService,
               public formBuilder: FormBuilder, public loadingCtrl: LoadingController,
               public alertCtrl: AlertController) {
+    this.tab = this.navCtrl.parent;
     this.signupForm = formBuilder.group({
       username: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
       email: ['', Validators.compose([Validators.required, ValidatorService.isValid])],
@@ -27,12 +29,14 @@ export class SignUpComponent {
   };
 
   signupUser(){
+    var self = this;
     if (!this.signupForm.valid){
       console.log(this.signupForm.value);
     } else {
       this.auth.signupUser(this.signupForm.value.email, this.signupForm.value.password, this.signupForm.value.username).then(() => {
         this.loading.dismiss().then(() => {
           this.navCtrl.setRoot(SessionComponent);
+          self.tab.select(0);
         });
       }, (error) => {
         this.loading.dismiss().then( () => {
